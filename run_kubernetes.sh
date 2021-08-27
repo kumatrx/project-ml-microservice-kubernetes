77188b1b46b5aa50c677da39caa3e930e96a8c3a
EC2 Default User <ec2-user@ip-172-31-31-179.ap-northeast-1.compute.internal>
2021-08-27 08:33:52 +0000

    First commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=adamsteff/app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-app-pod\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-app-pod
 
 # Step 3:
-# List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward ml-app-pod 8000:80
 
